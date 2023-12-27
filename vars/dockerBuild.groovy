fe8d5632655192f96f350f6fe835a67c05b5ae50
rushi <rushikeshnichit@gmail.com>
2023-12-28 00:25:40 +0530

    docker stage

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,3 @@
-def call(String project, String ImageTag, String hubUser){
-    bat """ 
-      docker image build -t ${hubUser}/${project} .
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-     """
+def call(){
+    bat "docker build -t javapp/app.jar ." 
 }
